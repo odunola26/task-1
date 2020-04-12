7ed1dc6906cee8187d3d21b2f6f9ced7f693c76e
odunola26 <62163826+odunola26@users.noreply.github.com>
2020-04-11 19:31:00 -0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var myname="Adebiyi Odunola"
-var mycourse=["html","css","javascript"]
-console.log(myname)
-console.log(mycourse)
-for (var i=0; i<=200; i++){
-    if (i%2 !==0) 
-    console.log(i);
-}
