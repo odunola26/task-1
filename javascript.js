ecd6dd8633a8802b80996772fb0ea1bd302e2be1
odunola26 <62163826+odunola26@users.noreply.github.com>
2020-04-11 19:31:33 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var myname="Adebiyi Odunola"
+var mycourse=["html","css","javascript"]
+console.log(myname)
+console.log(mycourse)
+for (var i=0; i<=200; i++){
+    if (i%2 !==0) 
+    console.log(i);
+}
